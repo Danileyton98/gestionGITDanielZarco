bb70fac3493d74d36021add563ffd22265a26d10
DANIEL\dani_ <dani_leyton@hotmail.com>
2023-09-23 14:31:17 +0200

    BaseDeDatosActualizada

--- HolaMundo/src/App.java
+++ HolaMundo/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int[] numeros = {34, 65, 234, 98, 93};
+        int numMayor = 0;
+
+        for(int i = 0; i < numeros.length; i++){
+            if(numMayor < numeros[i]){
+                numMayor = numeros[i];
+            }
     }
+    System.out.println(numMayor);
+}
 }
